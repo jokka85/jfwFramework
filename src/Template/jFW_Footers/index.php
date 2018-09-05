723be84491c3c7f8c1930729fb1bc954a831a46a
jokka85 <jokka85@gmail.com>
2018-09-05 05:46:50 -0500

    Update of footer links

---
+++ src/Template/jFW_Footers/index.php
@@ -0,0 +1,30 @@
+</div>
+<br/>
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-bottom mt-3 defaultFoot">
+
+            <a class="navbar-brand" href="https://github.com/jokka85/jfwFramework"><{APP_NAME}></a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bottomNavBar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="bottomNavBar">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/<{__BASE_DIR__}>/">Default Home Page</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="http://github.com/jokka85/jfwframework">Git Page</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="mailto:jokka85@gmail.com">Email jFW Framework</a>
+                    </li>
+                </ul>
+
+                <div class="nav navbar-nav navbar-right text-light">
+                    Runtime: <{runtime}>
+                </div>
+            </div>
+        </nav>
+    </body>
+</html>
